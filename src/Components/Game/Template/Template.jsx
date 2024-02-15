e9fdca0009ddf35b4ad961870209dd3797b7225c
mohamed-elaouan <mohamedelaouan6@gmail.com>
2024-02-15 18:38:22 +0100

    new branch

--- src/Components/Game/Template/Template.jsx
+++ src/Components/Game/Template/Template.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 
 const Template = () => {
@@ -7,5 +8,48 @@ const Template = () => {
     </div>
   );
 }
+=======
+import React, { useState } from "react";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
+import "./Template.scss";
+import PopUp from "./PopUp";
+import { images } from "../Template/Data/data.js";
+const Template = () => {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  console.log(images[0]);
+  return (
+    <div className="container-template">
+      <div className="container-template-ceontent">
+        <div className="images">
+          <img src={images[currentImage]} alt="#" />
+        </div>
+        <div className="content-template">
+          <div className="false-icons" onClick={handleShow}>
+            <FaTimesCircle />
+          </div>
+          <div
+            className="true-icons"
+            onClick={() => {
+              setCurrentImage((prev) =>
+                prev === images.length - 1 ? 0 : prev + 1
+              );
+            }}
+          >
+            <FaCheckCircle />
+          </div>
+        </div>
+      </div>
+      <PopUp show={show} handleClose={handleClose} />
+    </div>
+  );
+};
+>>>>>>> 51441946aa43941c35f98fcf3f4a324185e20745
 
 export default Template;
